281c7292ecea084721bb9f61e40a8fbdc23daf90
Josh Safran <joshsaf@gmail.com>
2017-04-12 15:43:28 -0400

    Initial commit with some skeleton GUI. Pods for network interaction and core data json mapping.

---
+++ SubtleVibes/GalleryDataSource.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryDataSource.swift
+//  SubtleVibes
+//
+//  Created by Josh Safran on 4/11/17.
+//  Copyright © 2017 Josh Safran. All rights reserved.
+//
+
+import Foundation
+
+class GalleryDataSource {
+    
+//    func GalleryDataSource(){
+//        
+//    }
+    
+//    var 
+    
+    
+}
